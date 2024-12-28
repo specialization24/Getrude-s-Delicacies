c8625ceecca0898f3445fa679965ca56f6c34960
ratc35ffe6253a <wangalibosamuel@yahoo.com>
2024-12-28 13:51:17 +0300

    frontend- footer- added fontawesome for icons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,32 +1,44 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        {/* Contact Section */}
         <div className="footer-section">
           <h4>Contact Us</h4>
           <p>Email: support@getrudesdelicacies.com</p>
           <p>Phone: +123 456 789</p>
         </div>
 
+        {/* Social Media Section */}
         <div className="footer-section">
           <h4>Follow Us</h4>
           <div className="social-links">
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-              Facebook
+              <FontAwesomeIcon icon={faFacebook} />
             </a>
             <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-              Twitter
+              <FontAwesomeIcon icon={faTwitter} />
             </a>
             <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-              Instagram
+              <FontAwesomeIcon icon={faInstagram} />
             </a>
           </div>
         </div>
 
-        <div className="footer-section">
+        {/* Links Section */}
+        <div className="footer-section links">
+          <a href="/about">About Us</a>
+          <a href="/contact">Contact</a>
+          <a href="/privacy-policy">Privacy Policy</a>
+        </div>
+
+        {/* Copyright Section */}
+        <div className="footer-section copyright">
           <p>&copy; {new Date().getFullYear()} Getrude's Delicacies. All Rights Reserved.</p>
         </div>
       </div>
