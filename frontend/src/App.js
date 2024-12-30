3cd20546e700c3335abff6c76407341534685f6b
ratc35ffe6253a <wangalibosamuel@yahoo.com>
2024-12-30 17:06:32 +0300

    styles- created a styles folder for layout to make footer stay at the bottom across all pages, Footer- changed the contents structure, implemented a div in App.js to handle styles in the styles folder.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 import Login from './pages/Login';
@@ -13,7 +14,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import PrivateRoute from './components/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
-import Footer from './components/Footer';
+import './styles/layout.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -31,40 +32,44 @@ function App() {
     <>
       <ToastContainer />
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home addToCart={addToCart} />} />
-          <Route path="/products" element={<ProductList addToCart={addToCart} />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/cart"
-            element={
-              <PrivateRoute>
-                <Cart cart={cart} clearCart={clearCart} />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/order-history"
-            element={
-              <PrivateRoute>
-                <OrderHistory />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/checkout"
-            element={
-              <PrivateRoute>
-                <Checkout cart={cart} clearCart={clearCart} />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-        <Footer />
+        <div className="app-container">
+          <Navbar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home addToCart={addToCart} />} />
+              <Route path="/products" element={<ProductList addToCart={addToCart} />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route
+                path="/cart"
+                element={
+                  <PrivateRoute>
+                    <Cart cart={cart} clearCart={clearCart} />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/order-history"
+                element={
+                  <PrivateRoute>
+                    <OrderHistory />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/checkout"
+                element={
+                  <PrivateRoute>
+                    <Checkout cart={cart} clearCart={clearCart} />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
       </Router>
     </>
   );
